d723a69f49094f488a7f268fb0a5695b20f1107f
domi <domi.hustinova@gmail.com>
2022-03-13 12:40:44 +0100

    feat: add not found and create pages

---
+++ src/components/common/common.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/macro'
+
+export const Wrapper = styled.div`
+  padding: 40px;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
---
+++ src/pages/Create.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { HEADER_THEME } from '../utils/consts'
+import { Wrapper } from '../components/common/common'
+import { Header } from '../components/Header/Header'
+import { HeaderContainer } from '../containers/Header'
+
+export function Create() {
+  return (
+    <Wrapper>
+      <HeaderContainer theme={HEADER_THEME.DARK}>
+        <Header.Close to="/dashboard">Close</Header.Close>
+      </HeaderContainer>
+    </Wrapper>
+  )
+}
